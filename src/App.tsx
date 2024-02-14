bbcc9803cc659a749ffa7bf5a5523ff4ad8cd53e
mattdinata <matt.dinata@gmail.com>
2024-02-14 13:46:53 +0800

    App: clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 import { signInWithPopup } from 'firebase/auth';
@@ -35,41 +33,14 @@ const handleClick = async (
 function App() {
   return (
     <>
-      <div>
-        <a
-          href='https://vitejs.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img
-            src={viteLogo}
-            className='logo'
-            alt='Vite logo'
-          />
-        </a>
-        <a
-          href='https://react.dev'
-          target='_blank'
-          rel='noreferrer'
-        >
-          <img
-            src={reactLogo}
-            className='logo react'
-            alt='React logo'
-          />
-        </a>
-      </div>
       <h1>DIP E028</h1>
       <div className='card'>
         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
         <button onClick={handleClick}>REGISTER WITH GOOGLE</button>
-        <p>
+        <p className='mt-4'>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
