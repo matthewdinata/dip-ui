54c81a7bdf061e9b32cb58806037979d390aa9ca
wk981 <jamesim445@gmail.com>
2024-03-11 21:34:45 +0800

    Added scroll to end function to chatbot page

--- src/pages/ChatbotPage/index.tsx
+++ src/pages/ChatbotPage/index.tsx
@@ -1,15 +1,15 @@
-import Chatboticon from "@/assets/Chatboticon.svg"
+import ChatbotIcon from "@/assets/ChatbotIcon.svg"
 import { IoMdPerson } from "react-icons/io";
 import { FaCircleChevronRight } from "react-icons/fa6";
-import { Message } from "./Components/Messages"
+import { Message } from "./components/Messages"
 import Search, { SearchProps } from "antd/es/input/Search";
-import { useState } from "react";
-import { MessageProps } from "./Components/Messages";
+import { useEffect, useRef, useState } from "react";
+import { MessageProps } from "./components/Messages";
 
 // Mock data
 const mockMessages = [
   {
-    ProfilePic: Chatboticon,
+    ProfilePic: ChatbotIcon,
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
@@ -17,7 +17,7 @@ const mockMessages = [
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
-    ProfilePic: Chatboticon,
+    ProfilePic: ChatbotIcon,
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
@@ -25,7 +25,7 @@ const mockMessages = [
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
-    ProfilePic: Chatboticon,
+    ProfilePic: ChatbotIcon,
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
@@ -33,7 +33,7 @@ const mockMessages = [
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
-    ProfilePic: Chatboticon,
+    ProfilePic: ChatbotIcon,
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
@@ -41,7 +41,7 @@ const mockMessages = [
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
-    ProfilePic: Chatboticon,
+    ProfilePic: ChatbotIcon,
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
@@ -49,19 +49,20 @@ const mockMessages = [
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
   {
-    ProfilePic: Chatboticon,
+    ProfilePic: ChatbotIcon,
     Message: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero nam nisi expedita ducimus suscipit deserunt odit debitis sunt hic ad tenetur laudantium fugiat perspiciatis fuga, ipsam, omnis dolorem ipsum. At?"
   },
 ]
 
 // A prototype of chatbotpage
 export const ChatbotPage = () => {
-  
-
   // Array of messages
   const [messages, setMessages] = useState<MessageProps[]>(mockMessages);
   // State for input
-  const [inputValue, setInputValue] = useState('')
+  const [inputValue, setInputValue] = useState('');
+
+  // ref for messageContainerDiv
+  const messageContainerRef = useRef<HTMLDivElement | null>(null);
 
   // handleSearch input
   const handleSearch: SearchProps['onSearch'] = (value, e) => {
@@ -78,11 +79,22 @@ export const ChatbotPage = () => {
       );
       // Clear input
       setInputValue('');
+      
       // Todo: Intergration of backend api to here
     }
-
-
   }
+
+  // When messge state is update, the user will be dragged to the bottom of chat
+  useEffect(() =>{
+    // Scroll to the bottom
+    messageContainerRef.current?.scrollIntoView({ block: 'end',behavior: 'smooth'});
+  },[messages]);
+
+  // When the component is mounted, the view of the chat will be at the bottom of the scroll
+  useEffect(() =>{
+    // Scroll to the bottom
+    messageContainerRef.current?.scrollIntoView({ block: 'end'});
+  },[]);
 
   return (
     // Todo: Add navbar
@@ -90,9 +102,9 @@ export const ChatbotPage = () => {
       <div className="flex flex-col gap-9 w-full h-full justify-center items-center overflow-hidden py-8">
         <div className="flex-1 w-full h-full flex justify-center items-center overflow-y-auto px-2">
           <div className="min-w-[330px] w-3/5 h-full">
-            <div className="w-full flex flex-col gap-7 my-4 flex-1">
+            <div ref={messageContainerRef} className="w-full flex flex-col gap-7 my-4 flex-1">
               {/* Message compnent will be mapped from state */}
-              {messages.map(message => <Message ProfilePic={message.ProfilePic} Message={message.Message} />)}
+              {messages.map((message, i) => <Message key={i} ProfilePic={message.ProfilePic} Message={message.Message} />)}
             </div>
           </div>
         </div>
