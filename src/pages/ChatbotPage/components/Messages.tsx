047de4cfb04d069b7975929191e664e9be4ea68a
mattdinata <matt.dinata@gmail.com>
2024-03-15 21:17:45 +0800

    root: run prettier write

--- src/pages/ChatbotPage/components/Messages.tsx
+++ src/pages/ChatbotPage/components/Messages.tsx
@@ -1,19 +1,17 @@
-export interface MessageProps{
-    profilePic: string;
-    message: string;
+export interface MessageProps {
+	profilePic: string;
+	message: string;
 }
 
-export const Message = ({profilePic, message}: MessageProps) => {
-    return (
-        <div className={`flex gap-4 `}>
-            {/* Profile */}
-            <div className="rounded-full border p-1 w-10 h-10 flex items-center justify-center bg-white">
-                <img src={profilePic} />
-            </div>
-            {/* Message */}
-            <div className="w-11/12 px-1 flex-1" >
-                {message}
-            </div>
-        </div>
-    )
-}
+export const Message = ({ profilePic, message }: MessageProps) => {
+	return (
+		<div className={`flex gap-4 `}>
+			{/* Profile */}
+			<div className="rounded-full border p-1 w-10 h-10 flex items-center justify-center bg-white">
+				<img src={profilePic} />
+			</div>
+			{/* Message */}
+			<div className="w-11/12 px-1 flex-1">{message}</div>
+		</div>
+	);
+};
--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,72 +1,76 @@
 // Constants - utils
-import { auth, db, GoogleProvider } from '@/services/firebase';
-import { signInWithPopup } from 'firebase/auth';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
-import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db, GoogleProvider } from "@/services/firebase";
+import { signInWithPopup } from "firebase/auth";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export default function LandingPage() {
-  const [user] = useAuthState(auth);
+	const [user] = useAuthState(auth);
 
-  const signInWithGoogle = async () => {
-    try {
-      const res = await signInWithPopup(auth, GoogleProvider);
-      // before making a new doc, check if doc already exists to prevent updating the old doc
-      const docRef = doc(db, 'users', res.user.uid);
-      const docSnap = await getDoc(docRef);
-      if (!docSnap.exists()) {
-        await setDoc(doc(db, 'users', res.user.uid), {
-          uid: res.user.uid,
-          email: res.user.email,
-          displayName: res.user.displayName,
-          profilePic: res.user.photoURL,
-        });
-      }
-    } catch (error) {
-      console.error('Error signing in with Google: ', error);
-    }
-  };
+	const signInWithGoogle = async () => {
+		try {
+			const res = await signInWithPopup(auth, GoogleProvider);
+			// before making a new doc, check if doc already exists to prevent updating the old doc
+			const docRef = doc(db, "users", res.user.uid);
+			const docSnap = await getDoc(docRef);
+			if (!docSnap.exists()) {
+				await setDoc(doc(db, "users", res.user.uid), {
+					uid: res.user.uid,
+					email: res.user.email,
+					displayName: res.user.displayName,
+					profilePic: res.user.photoURL,
+				});
+			}
+		} catch (error) {
+			console.error("Error signing in with Google: ", error);
+		}
+	};
 
-  const handleLogout = async () => {
-    try {
-      await auth.signOut();
-    } catch (error) {
-      console.error('Error signing out: ', error);
-    }
-  };
+	const handleLogout = async () => {
+		try {
+			await auth.signOut();
+		} catch (error) {
+			console.error("Error signing out: ", error);
+		}
+	};
 
-  return (
-    <>
-      <div>
-        {user ? (
-          <div>
-            <div className='mb-24 flex flex-col gap-2 items-center'>
-              <div className='mx-8 mb-4'>
-                <img
-                  src={user?.photoURL || ''}
-                  referrerPolicy='no-referrer'
-                  alt="User's Profile Picture"
-                  className='rounded-xl'
-                />
-              </div>
-              <div className='text-5xl font-semibold mb-2'>
-                Hello, {user?.displayName}
-              </div>
-              <div className='text-xl italic'>
-                You logged in using{' '}
-                <span className='text-emerald-700'>{user.email}</span>
-              </div>
-            </div>
-            <button onClick={handleLogout}>LOG OUT</button>
-          </div>
-        ) : (
-          <div>
-            <div className='mb-8 text-5xl font-semibold'>
-              Welcome to our Super App!
-            </div>
-            <button onClick={signInWithGoogle}>SIGN IN WITH GOOGLE</button>
-          </div>
-        )}
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				{user ? (
+					<div>
+						<div className="mb-24 flex flex-col gap-2 items-center">
+							<div className="mx-8 mb-4">
+								<img
+									src={user?.photoURL || ""}
+									referrerPolicy="no-referrer"
+									alt="User's Profile Picture"
+									className="rounded-xl"
+								/>
+							</div>
+							<div className="text-5xl font-semibold mb-2">
+								Hello, {user?.displayName}
+							</div>
+							<div className="text-xl italic">
+								You logged in using{" "}
+								<span className="text-emerald-700">
+									{user.email}
+								</span>
+							</div>
+						</div>
+						<button onClick={handleLogout}>LOG OUT</button>
+					</div>
+				) : (
+					<div>
+						<div className="mb-8 text-5xl font-semibold">
+							Welcome to our Super App!
+						</div>
+						<button onClick={signInWithGoogle}>
+							SIGN IN WITH GOOGLE
+						</button>
+					</div>
+				)}
+			</div>
+		</>
+	);
 }
