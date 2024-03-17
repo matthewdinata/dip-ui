b9212ace78bcf3a0170b2708d178e618a9d0f208
mattdinata <matt.dinata@gmail.com>
2024-03-17 20:42:15 +0800

    News: refactor section

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,88 +1,14 @@
 // Components
 import GameSection from "./components/GameSection";
-
-// Assets
-import { IoIosArrowForward } from "react-icons/io";
-import newsThumbnail from "@/assets/newsThumbnail@3x.png";
-
-// Constants - utils
-import { useNavigate } from "react-router-dom";
-
-const InsightThumbnail = ({
-	title,
-	thumbnailImage,
-	imageClassName,
-}: {
-	title: string;
-	thumbnailImage: string;
-	imageClassName?: string;
-}) => {
-	return (
-		<a href={"https://example.com/news"} target="_blank" rel="noreferrer">
-			<div className="h-40 w-56 rounded-3xl overflow-hidden relative">
-				<div className="bg-blue-200 h-full w-full">
-					<img className={imageClassName} src={thumbnailImage} />
-				</div>
-				<div className="absolute bottom-0 text-xs text-white bg-gray-800 bg-opacity-50 px-4">
-					{title}
-				</div>
-			</div>
-		</a>
-	);
-};
+import NewsSection from "./components/NewsSection";
 
 export default function DashboardPage() {
-	// Handle navigation
-	const navigate = useNavigate();
-	const handleNavigateToNewsPage = () => {
-		navigate("/news");
-	};
 	return (
 		<div className="min-h-screen">
-			<div className="mb-12 pt-24 flex flex-col gap-3">
-				<div className="flex justify-between items-end">
-					<div className="md:text-2xl text-xl font-bold text-start">
-						Insight Hub
-					</div>
-					<div
-						className="flex items-center gap-1 cursor-pointer"
-						onClick={handleNavigateToNewsPage}
-					>
-						<span className="font-medium text-gray-500">More</span>
-						<IoIosArrowForward className="text-sm mt-[0.1rem] text-gray-500" />
-					</div>
-				</div>
-				<div className="flex flex-row gap-4 items-center bg-black bg-opacity-25 p-8 rounded-2xl">
-					<InsightThumbnail
-						title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, optio consequuntur."
-						thumbnailImage={newsThumbnail}
-						imageClassName="h-[105%] w-[105%]"
-					/>
-					<InsightThumbnail
-						title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, optio consequuntur."
-						thumbnailImage={newsThumbnail}
-						imageClassName="h-[105%] w-[105%]"
-					/>
-					<InsightThumbnail
-						title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, optio consequuntur."
-						thumbnailImage={newsThumbnail}
-						imageClassName="h-[105%] w-[105%]"
-					/>
-					<InsightThumbnail
-						title="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, optio consequuntur."
-						thumbnailImage={newsThumbnail}
-						imageClassName="h-[105%] w-[105%]"
-					/>
-
-					<IoIosArrowForward className="text-3xl" />
-				</div>
-				<div className="text-center md:text-base sm:text-sm text-xs">
-					Go back to reading{" "}
-					<span className="underline text-red-800 font-medium">
-						Vaping is Harmful
-					</span>
-				</div>
+			<div className="mb-12">
+				<NewsSection />
 			</div>
+
 			<div>
 				<GameSection />
 			</div>
