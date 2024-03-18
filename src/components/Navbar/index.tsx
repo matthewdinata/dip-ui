a47d4928bbad596dd57d06edc60a9b3c5cbb357c
mattdinata <matt.dinata@gmail.com>
2024-03-18 21:20:18 +0800

    Navbar: implement login and logout

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,11 +2,10 @@
 import appLogo from "@/assets/appLogo@3x.png";
 
 // Constants - utils
-import { auth } from "@/services/firebase";
-import { useAuthState } from "react-firebase-hooks/auth";
+import useAuth from "@/hooks/useAuth";
 
 export default function Navbar() {
-	const [user] = useAuthState(auth);
+	const { user, login, logout } = useAuth();
 	return (
 		<div className="w-full h-12 bg-white drop-shadow-md grid grid-cols-3 gap-x-4 items-center px-4">
 			<div className="" />
@@ -18,12 +17,20 @@ export default function Navbar() {
 				{user ? (
 					<div className="flex h-8 items-center gap-2">
 						<div className="rounded-full w-7 h-7 aspect-square bg-red-500"></div>
-						<div className="bg-red-50 hover:bg-red-100 transition-all text-black text-xs md:text-sm font-medium py-1 px-2 sm:px-5 rounded-xl cursor-pointer">
+						<div
+							className="bg-red-100 hover:bg-red-200 transition-all text-black text-xs md:text-sm font-medium py-1 px-2 sm:px-5 rounded-xl cursor-pointer"
+							onClick={logout}
+						>
 							LOGOUT
 						</div>
 					</div>
 				) : (
-					<div>LOGIN</div>
+					<div
+						className="bg-orange-100 hover:bg-yellow-300 transition-all text-black text-xs md:text-sm font-medium py-1 px-2 sm:px-5 rounded-xl cursor-pointer"
+						onClick={login}
+					>
+						LOGIN
+					</div>
 				)}
 			</div>
 		</div>
