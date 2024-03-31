2c05dbc529f445d4a6b1801e50ef951ed42baf19
wk981 <jamesim445@gmail.com>
2024-03-31 19:56:09 +0800

    Optimised a little bit for article page

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,12 +1,29 @@
 export const fetchNews = async (topic: string, current_date: string) => {
-	const URL = import.meta.env.VITE_BACKEND_URL + "fetch_metadata?" + new URLSearchParams({
-		topic: topic,
-		current_date: current_date,
-	}).toString();
+	const URL =
+		import.meta.env.VITE_BACKEND_URL +
+		"fetch_metadata?" +
+		new URLSearchParams({
+			topic: topic,
+			current_date: current_date,
+		}).toString();
 	const response = await fetch(URL);
 
 	if (!response.ok) {
-		throw new Error('Failed to fetch news');
+		throw new Error("Failed to fetch news");
 	}
 	return response.json();
+};
+
+export const fetchArticle = async (url: string) => {
+	// Fetch the news content from the provided URL
+	const response = await fetch(
+		"https://asia-southeast2-ntu-eee-dip-e028.cloudfunctions.net/dip-backend-functions/fetch_article?url=" +
+			encodeURIComponent(url)
+	);
+	if (!response.ok) {
+		throw new Error("Failed to fetch article");
+	}
+	const newsContent = await response.text();
+	return newsContent;
+	// const newsContent = await response.text();
 };
--- src/pages/ArticlePage/index.tsx
+++ src/pages/ArticlePage/index.tsx
@@ -1,38 +1,75 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useQuery } from "react-query";
+import { useSearchParams } from "react-router-dom";
+import { fetchArticle } from "@/api";
+import { Spinner } from "@/components/Spinner";
+import { useToast } from "@/hooks/useToast";
 
 interface ParsedContentType {
-  news_article: string;
+	news_article: string;
 }
 
 const ArticlePage = () => {
-  const location = useLocation();
-  const queryParams: URLSearchParams = new URLSearchParams(location.search);
-  const title: string = queryParams.get("title") || "";
-  const urlToImage: string = queryParams.get("urlToImage") || "";
-  const publishedAt: string = queryParams.get("publishedAt") || "";
+	// Get the searchparams from the URL
+	const [searchParams] = useSearchParams();
+	const url = searchParams.get("url");
+	const title = searchParams.get("title");
+	const urlToImage = searchParams.get("urlToImage");
+	const publishedAt = searchParams.get("publishedAt");
 
-  const newsContent: string | null = queryParams.get("content");
-  const parsedContent: ParsedContentType | null = newsContent ? JSON.parse(newsContent) as ParsedContentType : null;
+	const { data, isLoading, isError } = useQuery("article", () =>
+		fetchArticle(url ?? "")
+	) || { data: null, isLoading: true, isError: false };
 
-  useEffect(() => {
-    window.scrollTo(0, 0); // Scroll to the top of the page when component mounts
-  }, []);
-  
-  return (
-    <div className="min-h-screen">
+	// Get ToastCreate function from hook
+	const { ToastCreate } = useToast();
+
+	useEffect(() => {
+		window.scrollTo(0, 0); // Scroll to the top of the page when component mounts
+	}, []);
+
+	if (isLoading) {
+		return (
+			<div className="elative w-full h-[calc(100vh-3.5rem)] py-2 flex items-center justify-center">
+				<Spinner />
+			</div>
+		);
+	}
+
+	if (isError) {
+		ToastCreate({
+			message: "Fail to fetch article",
+			description: "Something went wrong with fetching the article",
+			placement: "topRight",
+			toastType: "error",
+		});
+	}
+
+	const parsedContent: ParsedContentType | null = data
+		? (JSON.parse(data) as ParsedContentType)
+		: null;
+
+	return (
+		<div className="min-h-screen">
 			<div className="flex flex-col pb-12 pt-8 px-4 md:pb-12 md:pt-24 md:px-20 gap-8">
-				<div className="text-3xl md:text-5xl font-bold text-center md:text-center">{title}</div>
-        <img
-					src={urlToImage}
-					alt={title}
+				<div className="text-3xl md:text-5xl font-bold text-center md:text-center">
+					{title}
+				</div>
+				<img
+					src={urlToImage || ""}
+					alt={title || ""}
 					className="flex justify-center h-auto w-full"
 				/>
-        <h3>Published at: {publishedAt.split('T')[0]}</h3>
-        <p className="text-base md:text-2xl text-justify" style={{ lineHeight: '2' }}>{parsedContent?.news_article}</p>
-      </div>
-    </div>
-  );
+				<h3>Published at: {publishedAt?.split("T")[0]}</h3>
+				<p
+					className="text-base md:text-2xl text-justify"
+					style={{ lineHeight: "2" }}
+				>
+					{parsedContent?.news_article}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default ArticlePage;
