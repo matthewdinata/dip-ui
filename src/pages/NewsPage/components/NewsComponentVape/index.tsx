cf5e5ac0e8c70939ff6f137b143a7588cd1cfb51
SWanYin <158145365+SWanYin@users.noreply.github.com>
2024-03-31 13:45:30 +0800

    Created NewsPage/component/NewsComponentVape/index.tsx

---
+++ src/pages/NewsPage/components/NewsComponentVape/index.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
+
+export const NewsComponentVape: React.FC = () => {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const { data, isLoading, isError } = useQuery('vape', () => fetchNews('vape', '2024-03-29')) || {};
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
+	if (isError) {
+		return <div>Error: Failed to fetch news</div>;
+	}
+
+	return (	 
+		<div>
+		{/* eslint-disable-next-line */}
+		{data && data?.metadata.map((article: NewsItemProps) => (
+			<NewsItem
+			key={article.url}
+			title={article.title}
+			urlToImage={article.urlToImage}
+			url={article.url}
+			publishedAt={article.publishedAt}
+			/>
+		))}
+		</div>
+	);
+};
