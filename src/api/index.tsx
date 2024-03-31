b6c5a30dcb396db010f0d719580d132cf6feec43
SWanYin <158145365+SWanYin@users.noreply.github.com>
2024-03-31 15:26:26 +0800

    Update index.tsx

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -1,6 +1,3 @@
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
-import { useNavigate } from "react-router-dom";
-
 export const fetchNews = async (topic: string, current_date: string) => {
 	const response = await fetch(`https://asia-southeast2-ntu-eee-dip-e028.cloudfunctions.net/dip-backend-functions/fetch_metadata?topic=${topic}&current_date=${current_date}`);
 	if (!response.ok) {
